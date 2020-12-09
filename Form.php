2b48a886d51d108055e2778316ad85c2e616adf4
AmatMN <Lolhooft7>
2020-12-09 10:54:23 +0100

    update

--- Form.php
+++ Form.php
@@ -1,5 +1,5 @@
 <?php
-  require_once "../Settings.php";
+  require_once "Settings.php";
 
   $conn = new PDO("mysql:host=$servername;dbname=$database;", $username, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
