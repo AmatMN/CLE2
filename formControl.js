fe6c6c4a34fc08fe87ecfc77ee4e5ee4780e5808
AmatMN <Lolhooft7>
2020-12-16 00:59:17 +0100

    Made the show / hide js work, only styling left.

--- formControl.js
+++ formControl.js
@@ -1,8 +1,8 @@
+let tabs = document.getElementsByClassName("tab");
 let currentTab = 0;
 showTab(currentTab);
 
 function showTab(n) {
-    let tabs = document.getElementsByClassName("tab");
     tabs[n].style.display = "block";
 
     if (n === 0) {
@@ -16,4 +16,30 @@ function showTab(n) {
     } else {
         document.getElementById("nextBtn").innerHTML = "Volgende";
     }
+}
+
+function nextPrev(n) {
+    if (n === 1 && !validate()) return false;
+
+    tabs[currentTab].style.display = "none";
+    currentTab = currentTab + n;
+
+    if (currentTab >= tabs.length) {
+        let forms = document.getElementsByTagName("form");
+        forms[0].submit();
+        return false;
+    }
+    showTab(currentTab);
+}
+
+function validate() {
+    let valid = true;
+    let inputs = tabs[currentTab].getElementsByTagName("input");
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].value === "") {
+            inputs[i].className += " invalid";
+            valid = false;
+        }
+    }
+    return valid;
 }
