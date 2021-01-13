4813b2d4d4b16d5a047e0782aa8b851ab4972fe3
AmatMN <Lolhooft7>
2021-01-13 13:29:28 +0100

    made the login page a proper html page

--- login/login.php
+++
@@ -1,8 +0,0 @@
-<title>Login Here!</title>
-<h1>Login Page</h1>
-<form action='finished.php' method='post'>
-    <p>Username:</p> <input type='text' name='usrnme' /><br>
-    <p>Password:</p> <input type='password' name='pwd' /><br>
-    <input type='hidden' name='page' value='0'>
-    <input type='submit' value='submit'>
-</form>
